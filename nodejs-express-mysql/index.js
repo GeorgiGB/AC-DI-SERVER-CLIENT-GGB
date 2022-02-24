4e2492ead82c7d4dd0ae1606b96658f29ea83224
GeorgiGB <57988693+GeorgiGB@users.noreply.github.com>
2022-02-24 11:56:07 +0100

    Update index.js

--- nodejs-express-mysql/index.js
+++ nodejs-express-mysql/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+//const cors = require("cors");
 
 app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
@@ -8,7 +9,10 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
-require("./app/routes/tutorial.routes.js")(app);
+//require("./app/routes/tutorial.routes.js")(app);
+//require("./app/routes/notes.routes.js")(app);
+require("./app/routes/login.routes.js")(app);
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
